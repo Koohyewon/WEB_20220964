92cf8dada2cfb228ef784ffd4c4d2e4fc8f1ffac
구혜원 <khw6746@naver.com>
2023-04-12 02:10:59 +0000

    웹 프로그래밍 6주차

---
+++ js/data_type.js
@@ -0,0 +1,9 @@
+let number = 5;
+let str = '문자열 입력';	//""도 묶음 가능
+let prime = 1.5123;
+let is_ok = true;	//참
+let is_not = false;	//거짓
+let undefi;	//변수 이름만, 초기화X
+let empty = null;	//비어있음	//null로 표기한 것과 초기화를 안한 것은 다르다.
+
+console.log(undefi, empty);	//여러 개 출력
